1c30de82b33f3cf0199854a006d2bfb325f30352
annalisegong <annalise.gong@cuw.edu>
2023-09-24 12:55:57 -0500

    Initial Commit

---
+++ CSC 490/CSC_490App.swift
@@ -0,0 +1,17 @@
+//
+//  CSC_490App.swift
+//  CSC 490
+//
+//  Created by Anna Gong on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CSC_490App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
